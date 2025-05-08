7ab340fae29962b71980e6bbecb18fbfc080c259
Mostafa Ahmed <127509453+mostafaahmedd2@users.noreply.github.com>
2025-05-08 22:27:29 +0300

    admin typo

--- AdminService/src/main/java/com/crepestrips/adminservice/model/Admin.java
+++ AdminService/src/main/java/com/crepestrips/adminservice/model/Admin.java
@@ -27,7 +27,7 @@ public class Admin {
 
     public void setId(String id) { this.id = id; }
 
-    public String getUserame() { return username; }
+    public String getUsername() { return username; }
     public void setUsername(String username) { this.username = username; }
 
     public String getPassword() { return password; }
