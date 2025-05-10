fd8794fb39d03ac005f277358103d639b146683f
Mostafa Ahmed <127509453+mostafaahmedd2@users.noreply.github.com>
2025-05-10 22:21:11 +0300

    container running to test basic crud

--- AdminService/src/main/java/com/crepestrips/adminservice/controller/AdminController.java
+++ AdminService/src/main/java/com/crepestrips/adminservice/controller/AdminController.java
@@ -1,22 +1,14 @@
 package com.crepestrips.adminservice.controller;
 
 import com.crepestrips.adminservice.model.Admin;
-//import com.crepestrips.adminservice.security.JwtService;
 import com.crepestrips.adminservice.service.AdminService;
-import com.crepestrips.adminservice.dto.LoginRequest;
-import com.crepestrips.adminservice.dto.LoginResponse;
 
 
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,14 +18,10 @@ import java.util.List;
 public class AdminController {
 
     private final AdminService adminService;
-    private final AuthenticationManager authenticationManager;
-//    private final JwtService jwtUtil;
 
     @Autowired
-    public AdminController(AdminService adminService, AuthenticationManager authenticationManager) {
+    public AdminController(AdminService adminService) {
         this.adminService = adminService;
-        this.authenticationManager = authenticationManager;
-//        this.jwtUtil = jwtUtil;
     }
 
     @PostMapping("/create")
@@ -87,12 +75,12 @@ public class AdminController {
 //        }
 //    }
 
-    @PostMapping("/logout")
-    public String logout(@RequestParam String username) {
-        // Logout logic (e.g., invalidate session, clear context, etc.)
-        SecurityContextHolder.clearContext();
-        return "Admin " + username + " logged out successfully.";
-    }
+//    @PostMapping("/logout")
+//    public String logout(@RequestParam String username) {
+//        // Logout logic (e.g., invalidate session, clear context, etc.)
+//        SecurityContextHolder.clearContext();
+//        return "Admin " + username + " logged out successfully.";
+//    }
 
 
 }
