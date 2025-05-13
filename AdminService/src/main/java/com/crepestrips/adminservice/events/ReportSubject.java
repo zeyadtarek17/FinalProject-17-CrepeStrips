07e16a45161fb0bbbfd5a16121aa5971850607cb
Mostafa Ahmed <127509453+mostafaahmedd2@users.noreply.github.com>
2025-05-13 22:13:05 +0300

    modified events package

--- AdminService/src/main/java/com/crepestrips/adminservice/events/ReportSubject.java
+++ AdminService/src/main/java/com/crepestrips/adminservice/events/ReportSubject.java
@@ -1,9 +1,9 @@
-package com.crepestrips.adminservice.events;
-
-import java.util.Observer;
-
-public interface ReportSubject {
-    void registerObserver(Observer o);
-    void removeObserver(Observer o);
-    void notifyObservers();
-}
+//package com.crepestrips.adminservice.events;
+//
+//import java.util.Observer;
+//
+//public interface ReportSubject {
+//    void registerObserver(Observer o);
+//    void removeObserver(Observer o);
+//    void notifyObservers();
+//}
