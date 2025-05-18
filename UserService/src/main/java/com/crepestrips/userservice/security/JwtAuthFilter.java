ebe8207b6d1e023af536e55eb8e37ed86c11d278
Adham Elsegeeny <adham_elsegeeny@icloud.com>
2025-05-18 19:06:31 +0300

    modify filter

--- UserService/src/main/java/com/crepestrips/userservice/security/JwtAuthFilter.java
+++ UserService/src/main/java/com/crepestrips/userservice/security/JwtAuthFilter.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -34,11 +33,6 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        String path = request.getServletPath();
-        if (path.equals("/api/user/register") || path.equals("/api/user/login")) {
-            filterChain.doFilter(request, response);
-            return;
-        }
         String authHeader = request.getHeader("Authorization");
         String token = null;
         String username = null;
