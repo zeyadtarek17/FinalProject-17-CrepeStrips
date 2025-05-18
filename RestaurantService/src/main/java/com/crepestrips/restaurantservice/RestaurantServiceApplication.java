7a0c452c80003dc828c28a8966e1b2de9ec61e3a
zeyad <eng.zeyadtarek@gmail.com>
2025-05-19 02:17:40 +0300

    APIGateway

--- RestaurantService/src/main/java/com/crepestrips/restaurantservice/RestaurantServiceApplication.java
+++ RestaurantService/src/main/java/com/crepestrips/restaurantservice/RestaurantServiceApplication.java
@@ -3,11 +3,13 @@ package com.crepestrips.restaurantservice;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableRabbit
+@EnableDiscoveryClient
 public class RestaurantServiceApplication {
 
 	public static void main(String[] args) {
