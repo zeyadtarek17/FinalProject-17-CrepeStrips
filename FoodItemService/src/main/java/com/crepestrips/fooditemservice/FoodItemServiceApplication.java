1d9dcc935c45a28027858e265d829e8fcda9d11c
Aly Rezk <aly.ahmed2002@gmail.com>
2025-05-19 23:13:56 +0300

    pull segeeny

--- FoodItemService/src/main/java/com/crepestrips/fooditemservice/FoodItemServiceApplication.java
+++ FoodItemService/src/main/java/com/crepestrips/fooditemservice/FoodItemServiceApplication.java
@@ -4,11 +4,13 @@ import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
 @EnableRabbit
 @EnableDiscoveryClient
+@EnableFeignClients
 public class FoodItemServiceApplication {
 
 	public static void main(String[] args) {
