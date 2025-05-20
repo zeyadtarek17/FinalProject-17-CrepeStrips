44b7b7a2bd81aeb39c21475823ed6e59f5e9d213
zeyad <eng.zeyadtarek@gmail.com>
2025-05-20 20:18:17 +0300

    error fix

--- FoodItemService/src/main/java/com/crepestrips/fooditemservice/FoodItemServiceApplication.java
+++ FoodItemService/src/main/java/com/crepestrips/fooditemservice/FoodItemServiceApplication.java
@@ -4,12 +4,9 @@ import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-
 
 @SpringBootApplication
 @EnableRabbit
-@EnableFeignClients
 @EnableDiscoveryClient
 public class FoodItemServiceApplication {
 
